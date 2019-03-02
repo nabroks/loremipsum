1debd11a6ba6b189df712528c0e7c6ef9c70b1a7
Paweł Ładna <skorban@gmail.com>
2019-03-02 11:17:17 +0100

    Initial commit

---
+++ src/main/java/skorban/loremipsum/GeneratorControler.java
@@ -0,0 +1,7 @@
+package skorban.loremipsum;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GeneratorControler {
+}
---
+++ src/main/java/skorban/loremipsum/LoremipsumApplication.java
@@ -0,0 +1,13 @@
+package skorban.loremipsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoremipsumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoremipsumApplication.class, args);
+    }
+
+}
