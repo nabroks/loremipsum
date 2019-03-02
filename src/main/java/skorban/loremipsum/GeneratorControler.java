904289cfb7c2f0fd07e618d5999f70323d729fa9
Paweł Ładna <skorban@gmail.com>
2019-03-02 14:48:11 +0100

    Poprawiono, dziala test, dzialamy dalej

--- src/main/java/skorban/loremipsum/GeneratorControler.java
+++ src/main/java/skorban/loremipsum/GeneratorControler.java
@@ -9,116 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GeneratorControler {
 
-    private LoremIpsum loremIpsum = new LoremIpsum();
-    private  Lorem lorem = new Lorem() {
-        @Override
-        public String getCity() {
-            return null;
-        }
+    private Lorem lorem = new LoremIpsum();
 
-        @Override
-        public String getCountry() {
-            return null;
-        }
-
-        @Override
-        public String getEmail() {
-            return null;
-        }
-
-        @Override
-        public String getFirstName() {
-            return null;
-        }
-
-        @Override
-        public String getFirstNameMale() {
-            return null;
-        }
-
-        @Override
-        public String getFirstNameFemale() {
-            return null;
-        }
-
-        @Override
-        public String getLastName() {
-            return null;
-        }
-
-        @Override
-        public String getName() {
-            return null;
-        }
-
-        @Override
-        public String getNameMale() {
-            return null;
-        }
-
-        @Override
-        public String getNameFemale() {
-            return null;
-        }
-
-        @Override
-        public String getTitle(int count) {
-            return null;
-        }
-
-        @Override
-        public String getTitle(int min, int max) {
-            return null;
-        }
-
-        @Override
-        public String getHtmlParagraphs(int min, int max) {
-            return null;
-        }
-
-        @Override
-        public String getParagraphs(int min, int max) {
-            return null;
-        }
-
-        @Override
-        public String getUrl() {
-            return null;
-        }
-
-        @Override
-        public String getWords(int count) {
-            return null;
-        }
-
-        @Override
-        public String getWords(int min, int max) {
-            return null;
-        }
-
-        @Override
-        public String getPhone() {
-            return null;
-        }
-
-        @Override
-        public String getStateAbbr() {
-            return null;
-        }
-
-        @Override
-        public String getStateFull() {
-            return null;
-        }
-
-        @Override
-        public String getZipCode() {
-            return null;
-        }
-    };
+    @GetMapping("/test")
+    public String testLorem(ModelMap modelMap) {
+        modelMap.put("test", lorem.getParagraphs(1, 10));
+        return "test";
+    }
 
     @GetMapping("/main")
     public String generatorLorem() {
-     return lorem.getHtmlParagraphs(1,10);
- }
+        return lorem.getHtmlParagraphs(1, 10);
+    }
+
 }
